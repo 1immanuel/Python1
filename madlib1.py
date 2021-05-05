31688899df62aec6a7b38fc43f00ae6cbaf8412f
Immanuel Alexander <i.alexander2012@yahoo.com>
2021-05-05 16:00:08 -0500

    adds project file

---
+++ madlib1.py
@@ -0,0 +1,7 @@
+print("The _(adj)_ flowers made _(name)_ very _(feeling)_.")
+
+adj = input("Enter an adjective: ")
+name = input("Enter a name: ")
+emotion = input("Enter an emotion: ")
+
+print("The %s flowers made %s very %s" % (adj, name, emotion))
