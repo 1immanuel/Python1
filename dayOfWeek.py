b9fad7117ded649d80b84d318a733575a6aa02db
Immanuel Alexander <i.alexander2012@yahoo.com>
2021-05-05 20:55:16 -0500

    needs halp

---
+++ dayOfWeek.py
@@ -0,0 +1,32 @@
+cero = "sunday"
+one = "monday"
+two = "tuesday"
+three = "wednesday"
+four = "thursday"
+five = "friday"
+six = "saturday"
+
+print("Choose a value 0-6.")
+input("Enter  value: ")
+
+if "0":
+    print("Today is %s." % (cero))
+elif "1":
+    print("Today is %s." % (one))
+    print("Go to work.")
+elif "2":
+    print("Today is %s." % (two))
+    print("Go to work.")
+elif "3":
+    print("Today is %s" % (three))
+    print("Go to work.")
+elif "4":
+    print("Today is %s" % (four))
+    print("Go to work.")
+elif "5": 
+    print("Today is %s" % (five))
+    print("Go to work)")
+elif "6":
+    print("Today is %s" % (six))
+else:
+    print("You\'re so clever -_-.")
