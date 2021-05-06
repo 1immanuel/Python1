ff0d3596e10bdf3d47292327293f255d9482ec38
Immanuel Alexander <i.alexander2012@yahoo.com>
2021-05-06 09:09:37 -0500

    not so helpless

--- dayOfWeek.py
+++ dayOfWeek.py
@@ -8,21 +8,21 @@ six = "saturday"
 work = "Go to work."
 
 print("Choose a value 0-6.")
-answer = input("Enter value: ")
+answer = int(input("Enter value: "))
 
-if answer == "0" or "zero":
+if answer == 0:
     print("Today is %s. Blessings." % (cero))
-elif answer == "1" or "one":
+elif answer == 1:
     print("Today is %s. %s." % (one, work))
-elif answer == "2" or "two":
+elif answer == 2:
     print("Today is %s. %s." % (two, work))
-elif answer == "3" or "three":
+elif answer == 3:
     print("Today is %s. %s." % (three, work))
-elif answer == "4" or "four":
+elif answer == 4:
     print("Today is %s. %s." % (four, work))
-elif answer == "5" or "five": 
+elif answer == 5: 
     print("Today is %s. %s." % (five, work))
-elif answer == "6" or "six":
+elif answer == 6:
     print("weekendVibes." % (six))
 else:
     print("You\'re so clever -_-.")
